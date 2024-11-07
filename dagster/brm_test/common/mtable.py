fdff65d9ae001f7558439d995642cf426b181f4f
gdtu-0 <gdtu-0@outlook.com>
2024-11-07 10:54:58 +0300

    refactoring

--- dagster/brm_test/common/mtable.py
+++ dagster/brm_test/common/mtable.py
@@ -1,25 +1,37 @@
 from pandas import DataFrame
 from typing import Optional
+from dataclasses import dataclass
 from dagster import ConfigurableResource
 from ..resources.postgres_db import PostgresDB
 
 
+@dataclass
+class TableDefinition:
+    """Table definition dataclass"""
+
+    name: str
+    column_definitions: dict
+
+    @property
+    def columns(self) -> tuple:
+        return tuple(self.column_definitions.keys())
+
 class MTable:
     """Base MTable class"""
 
-    resource: ConfigurableResource
-    name: str
-    columns: tuple[str]
+    _table_definition: TableDefinition
+    _resource: ConfigurableResource
 
-    def __init__(self, resource: ConfigurableResource):
-        self.resource = resource
+    def __init__(self, table_definition: TableDefinition, resource: ConfigurableResource):
+        self._table_definition = table_definition
+        self._resource = resource
 
     def create(self) -> None:
         """Base create method"""
 
         raise NotImplementedError
     
-    def insert(self) -> None:
+    def insert(self, data: DataFrame) -> None:
         """Base insert method"""
 
         raise NotImplementedError
@@ -33,32 +45,30 @@ class MTable:
 class MTable_PG(MTable):
     """MTable implementation for PostgreSQL"""
 
-    def create(self, name: str, columns: dict[str, str], data: Optional[DataFrame] = None) -> None:
+    def create(self) -> None:
         """PostgreSQL create method"""
         
-        self.name = name
-        self.columns = tuple(columns.keys())
-        
-        columns_str = ",\n  ".join(f'{name} {specs}' for name, specs in columns.items())
-        create_str = f"CREATE TABLE IF NOT EXISTS {self.name} (\n  {columns_str}\n)"
+        columns_str = ",\n  ".join(f'{name} {specs}' for name, specs in self._table_definition.column_definitions.items())
+        create_str = f"CREATE TABLE IF NOT EXISTS {self._table_definition.name} (\n  {columns_str}\n)"
         sql_str = f"{create_str};"
 
-        self.resource.exec_sql_no_fetch(sql_str)
+        self._resource.exec_sql_no_fetch(sql_str)
 
-    def insert(self, values: list[tuple]):
+    def insert(self, data: DataFrame):
         """PostgreSQL insert method"""
 
-        columns_str = ", ".join(name for name in self.columns)
-        insert_str = f"INSERT INTO {self.name}\n  ({columns_str})\nVALUES %s"
-        sql_str = f"{insert_str}"
-        self.resource.exec_insert(sql = sql_str, values = values)
+        columns_str = ", ".join(name for name in data.columns)
+        insert_str = f"INSERT INTO {self._table_definition.name}\n  ({columns_str})\nVALUES %s"
+        sql_str = insert_str
+        
+        self._resource.exec_insert(sql = sql_str, values = list(data.itertuples(index = False, name = None)))
 
 
-def generate_MTable(resource: ConfigurableResource) -> MTable:
+def generate_MTable(table_definition: TableDefinition, resource: ConfigurableResource) -> MTable:
     """MTable generator"""
 
     if resource.__class__ is PostgresDB:
-        mtable = MTable_PG(resource)
+        mtable = MTable_PG(table_definition, resource)
     else:
         raise NotImplementedError
     
--- dagster/brm_test/ops/load_data.py
+++ dagster/brm_test/ops/load_data.py
@@ -9,6 +9,7 @@ from ..common import TABLE_DEFINITIONS
 
 def _list_dir(relpath: str) -> list[str]:
     """List files in path (full filenames)"""
+
     out = []
     path = os.path.join(os.path.dirname(__file__), relpath)
     for file in os.listdir(path):
@@ -18,6 +19,7 @@ def _list_dir(relpath: str) -> list[str]:
 @op
 def load_sapmle_data(context: OpExecutionContext) -> None:
     """Load data sample as Dataframe"""
+
     relpath = '../data/source_data/'
     for filename in _list_dir(relpath):
         context.log.info(f"Loading {filename}")
@@ -26,14 +28,15 @@ def load_sapmle_data(context: OpExecutionContext) -> None:
 @op
 def load_mapping(context: OpExecutionContext, postgres_db: PostgresDB) -> None:
     """Load mapping file"""
+    
+    table = generate_MTable(table_definition = TABLE_DEFINITIONS['mapping'], resource = postgres_db)
+    table.create()
     relpath = '../data/mapping/'
-    table = generate_MTable(postgres_db)
-    table.create(name='mapping', columns=TABLE_DEFINITIONS['mapping'])
     for filename in _list_dir(relpath):
         context.log.info(f"Loading {filename}")
         df = pd.read_excel(filename, engine="odf")
         df = df.fillna(value='')
-        table.insert(values=list(df.itertuples(index = False, name = None)))
+        table.insert(df)
     
 
 @graph
