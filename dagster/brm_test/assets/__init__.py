0c1d088d308b45a793cdb35f8fd08cde58d349c7
gdtu-0 <gdtu-0@outlook.com>
2024-11-14 23:29:31 +0300

    start work on mapping apply

--- dagster/brm_test/assets/__init__.py
+++ dagster/brm_test/assets/__init__.py
@@ -4,8 +4,13 @@ from .load_source import (
     source_data_loaded
 )
 
+from .pg_apply_mapping import (
+    pg_apply_mapping,    
+)
+
 PROJECT_ASSETS = [
     load_mapping,
     load_sapmle_data,
     source_data_loaded,
+    pg_apply_mapping,
 ]
---
+++ dagster/brm_test/assets/pg_apply_mapping.py
@@ -0,0 +1,22 @@
+from dagster import asset, AssetExecutionContext
+from ..resources.postgres_db import PostgresDB
+from ..common.mtable import generate_MTable
+from ..common import TABLE_DEFINITIONS
+from .load_source import source_data_loaded
+
+
+@asset(deps=[source_data_loaded])
+def pg_apply_mapping(context: AssetExecutionContext, postgres_db: PostgresDB) -> None:
+    """Apply mapping for PostgreSQL DB"""
+
+    data_table = generate_MTable(table_definition = TABLE_DEFINITIONS['data'], resource = postgres_db)
+    mapping_table = generate_MTable(table_definition = TABLE_DEFINITIONS['mapping'], resource = postgres_db)
+
+    mapping_df = mapping_table.select()
+    for index, rule in mapping_df.iterrows():
+        result_df = data_table.select(where = rule['where_cond'])
+        if result_df is not None:
+            results_count = result_df.shape[0]
+        else:
+            results_count = 0
+        print(f"RULE {index}: WHERE {rule['where_cond']}\nRESULTS :{results_count} row(s)")
